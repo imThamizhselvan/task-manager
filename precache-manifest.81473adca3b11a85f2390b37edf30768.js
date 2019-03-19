ca81dcbcffa59b3cf4e171eb8b2e0ba2975999a2
thamizhselvan <thamizhtechie@gmail.com>
2019-03-19 14:24:28 +0530

    Updates

---
+++ precache-manifest.81473adca3b11a85f2390b37edf30768.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0792098e53bcfcde74a",
+    "url": "/task-manager/static/js/runtime~main.057b5d69.js"
+  },
+  {
+    "revision": "c4c55cb6ae2f40c178a2",
+    "url": "/task-manager/static/js/main.76385cfc.chunk.js"
+  },
+  {
+    "revision": "015040fba6465e6d758a",
+    "url": "/task-manager/static/js/2.857766c5.chunk.js"
+  },
+  {
+    "revision": "c4c55cb6ae2f40c178a2",
+    "url": "/task-manager/static/css/main.47b12ce0.chunk.css"
+  },
+  {
+    "revision": "6a35d10f96bf572ebb3100629e62aebb",
+    "url": "/task-manager/index.html"
+  }
+];
