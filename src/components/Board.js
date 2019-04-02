a447fdf2174ffb8d72de2d7baf66864c592708a3
thamizhselvan <thamizhtechie@gmail.com>
2019-04-02 23:23:38 +0530

    listing bug

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
 import List from './List';
+import { db } from '../firebase';
 import { Wrapper, BoardWrapper, SingleLineInput, MultiLineInput, AddComment, CloseIcon } from './styles';
 
 export default class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todo: [], 
+      wip: [],
+      comp: []
+    };
+   }
+
+  componentDidMount() {
+    console.log('this.state', this.state);
+    db.collection('tasks').where('status', '==', 'todo')
+    .onSnapshot((querySnapshot) => {    
+      querySnapshot.forEach((doc) => {
+        this.setState({
+          todo: [...this.state.todo, doc.data() ],
+        });
+      });
+    });
+    db.collection('tasks').where('status', '==', 'wip')
+    .onSnapshot((querySnapshot) => {    
+      querySnapshot.forEach((doc) => {
+        this.setState({
+          wip: [...this.state.wip, doc.data() ],
+        });
+      });
+    });
+    db.collection('tasks').where('status', '==', 'comp')
+    .onSnapshot((querySnapshot) => {    
+      querySnapshot.forEach((doc) => {
+        this.setState({
+          comp: [...this.state.comp, doc.data() ],
+        });
+      });
+    });
+  }
+
   render() {
+    console.log('this.state board', this.state);
     return (
         <BoardWrapper>
             <p> Task board </p>
-            <List title="To do" cat="todo" />
-            <List title="Work in Progress" cat="wip" />
-            <List title="Completed" cat="comp"/>
+            <List title="To do" tasks={this.state.todo} cat="todo" />
+            <List title="Work in Progress" tasks={this.state.wip} cat="wip" />
+            <List title="Completed" tasks={this.state.comp} cat="comp"/>
         </BoardWrapper>
     );
   }
--- src/components/List.js
+++ src/components/List.js
@@ -17,89 +17,20 @@ export default class List extends Component {
     super();
     this.state = {
       modalIsOpen: false,
-      todo: [], 
-      wip: [],
-      comp: [],
-      currentDrag: []
     };
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
 
-  componentDidMount() {
-    console.log('this.state', this.state);
-    db.collection('tasks').where('status', '==', 'todo')
-    .onSnapshot((querySnapshot) => {    
-      querySnapshot.forEach((doc) => {
-        this.setState({
-          todo: [...this.state.todo, doc.data() ],
-        });
-      });
-    });
-    db.collection('tasks').where('status', '==', 'wip')
-    .onSnapshot((querySnapshot) => {    
-      querySnapshot.forEach((doc) => {
-        this.setState({
-          wip: [...this.state.wip, doc.data() ],
-        });
-      });
-    });
-    db.collection('tasks').where('status', '==', 'comp')
-    .onSnapshot((querySnapshot) => {    
-      querySnapshot.forEach((doc) => {
-        this.setState({
-          comp: [...this.state.comp, doc.data() ],
-        });
-      });
-    });
-  }
-  
   openModal() {
     this.setState({modalIsOpen: true});
   }
-
+  
   closeModal() {
     this.setState({modalIsOpen: false});
   }
 
   render() {
-
-    var tasks = { 
-      todo: [], 
-      wip: [],
-      comp: []    
-    }   
-    console.log('render', this.state);
-    this.state.todo.forEach((task) => {
-      tasks[task.status].push(
-        <CardDescWrapper 
-          key={task.title}
-        > 
-          <p> {task.title} </p>
-          <p> {task.desc} </p>
-        </CardDescWrapper>
-      );
-    })
-    this.state.wip.forEach((task) => {
-      tasks[task.status].push(
-        <CardDescWrapper 
-          key={task.title}
-        > 
-          <p> {task.title} </p>
-          <p> {task.desc} </p>
-        </CardDescWrapper>
-      );
-    })
-    this.state.comp.forEach((task) => {
-      tasks[task.status].push(
-        <CardDescWrapper 
-          key={task.title}
-        > 
-          <p> {task.title} </p>
-          <p> {task.desc} </p>
-        </CardDescWrapper>
-      );
-    })
     return (
       <ListWrapper>
         <CloseIcon><i class="fa fa-trash"></i></CloseIcon>
@@ -110,12 +41,16 @@ export default class List extends Component {
           onRequestClose={this.closeModal}
           style={customStyles}
           ariaHideApp={false}
+          status={this.props.cat}
         >
           <Card closeModal={this.closeModal} status={this.props.cat} />
         </Modal>
-        {this.props.cat == 'todo' && <div>{tasks.todo}</div>}
-        {this.props.cat === 'wip' && <div>{tasks.wip}</div>}
-        {this.props.cat === 'comp' && <div>{tasks.comp}</div>}
+        { this.props.tasks.map((task) =>
+          <CardDescWrapper> 
+            <p> {task.title} </p>
+            <p> {task.desc} </p>
+          </CardDescWrapper>    
+        )}
       </ListWrapper>
     );
   }
