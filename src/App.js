7161e5c366bc108cc938cb6662df792345519dff
thamizhselvan <thamizhtechie@gmail.com>
2019-04-04 16:15:06 +0530

    added github auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Board from './components/Board';
+import Login from './containers/Login';
+import history from './history';
+import { Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <Board />
+      <Router history={history}>
+        <Route exact path="/"  component={Login} />
+        <Route path="/task" component={Board} />
+      </Router>
     );
   }
 }
---
+++ src/containers/Login/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const InfoArea = styled.div`
+  width: 70%;
+  height: 100%;
+  display: inline-block;
+  color: #fff;
+  background-size: cover;
+  font-size: 6em;
+  background: url('https://github.com/images/modules/site/heroes/simple-codelines.svg'), #2a3137;
+  text-align: center;
+  padding-top: 10%;
+`;
+
+export const LoginArea = styled.div`
+  width: 30%;
+  height: 100%;
+  display: inline-block;
+  float: right;
+  bottom: 10px;
+  background-color: #ffffff;
+  padding-top: 10%;
+`;
+
+export const LoginWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Img = styled.img`
+  width: 90%;
+  height: 30%;
+`;
